633257cd228fbdd8776ccdeca8a1e2de92c7cd7d
Andrei-Marius Radu <andrei@zededa.com>
2026-03-12 10:25:11 +0100

    wip: Add no_dhcp_no_ip network config in modularized enterprise module

--- examples/other/modularized/modules/enterprise/networks.tf
+++ examples/other/modularized/modules/enterprise/networks.tf
@@ -12,3 +12,17 @@ resource "zedcloud_network" "default_network_dhcp_client" {
 
   mtu = 1500
 }
+
+resource "zedcloud_network" "no_dhcp_no_ip" {
+  name  = "no_dhcp_no_ip"
+  title = "A network object without DHCP or IP config"
+  kind  = "NETWORK_KIND_V4_ONLY"
+
+  project_id = zedcloud_project.this.id
+
+  ip {
+    dhcp = "NETWORK_DHCP_TYPE_PASSTHROUGH"
+  }
+
+  mtu = 1500
+}
