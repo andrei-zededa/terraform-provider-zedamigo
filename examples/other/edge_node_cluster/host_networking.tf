c9c11f58360df2260afe6a90b5e69c799ce81a5e
Andrei-Marius Radu <andrei@zededa.com>
2026-05-07 16:05:03 +0300

    wip: Add ENC starter config

---
+++ examples/other/edge_node_cluster/host_networking.tf
@@ -0,0 +1,153 @@
+resource "zedamigo_bridge" "BRIDGE_A" {
+  name         = "brA-${var.config_suffix}"
+  mtu          = "1500"
+  state        = "up"
+  ipv4_address = "10.99.1.1/24"
+}
+
+resource "zedamigo_dhcp_server" "DHCP_A" {
+  interface  = zedamigo_bridge.BRIDGE_A.name
+  server_id  = "10.99.1.1"
+  nameserver = "9.9.9.9"
+  router     = "10.99.1.1"
+  netmask    = "255.255.255.0"
+  pool {
+    start = "10.99.1.50"
+    end   = "10.99.1.59"
+  }
+  lease_time = 86400
+}
+
+resource "zedamigo_tap" "TAP_A_1" {
+  name   = "tapA-1-${var.config_suffix}"
+  mtu    = "1500"
+  state  = "up"
+  group  = "kvm"
+  master = zedamigo_bridge.BRIDGE_A.name
+}
+
+resource "zedamigo_tap" "TAP_A_2" {
+  name   = "tapA0-${var.config_suffix}"
+  mtu    = "1500"
+  state  = "up"
+  group  = "kvm"
+  master = zedamigo_bridge.BRIDGE_A.name
+}
+
+resource "zedamigo_tap" "TAP_A_3" {
+  name   = "tapA-3-${var.config_suffix}"
+  mtu    = "1500"
+  state  = "up"
+  group  = "kvm"
+  master = zedamigo_bridge.BRIDGE_A.name
+}
+
+resource "zedamigo_netns" "TEST_NS_B" {
+  name = "TEST_NS_B"
+}
+
+resource "zedamigo_bridge" "BRIDGE_B" {
+  name         = "brB-${var.config_suffix}"
+  mtu          = "1500"
+  state        = "up"
+  ipv4_address = "10.99.2.1/24"
+  netns        = zedamigo_netns.TEST_NS_B.name
+}
+
+resource "zedamigo_dhcp_server" "DHCP_B" {
+  depends_on = [zedamigo_edge_node.ENODE_001] # Because the TAPs are only moved inside the NS after the QEMU process starts.
+
+  interface  = zedamigo_bridge.BRIDGE_B.name
+  server_id  = "10.99.2.1"
+  nameserver = "9.9.9.9"
+  router     = "10.99.2.1"
+  netmask    = "255.255.255.0"
+  pool {
+    start = "10.99.2.60"
+    end   = "10.99.2.69"
+  }
+  lease_time = 86400
+  netns      = zedamigo_netns.TEST_NS_B.name
+}
+
+resource "zedamigo_tap" "TAP_B_1" {
+  name   = "tapB-1-${var.config_suffix}"
+  mtu    = "1500"
+  state  = "up"
+  group  = "kvm"
+  master = zedamigo_bridge.BRIDGE_B.name
+  netns  = zedamigo_netns.TEST_NS_B.name
+}
+
+resource "zedamigo_tap" "TAP_B_2" {
+  name   = "tapB-2-${var.config_suffix}"
+  mtu    = "1500"
+  state  = "up"
+  group  = "kvm"
+  master = zedamigo_bridge.BRIDGE_B.name
+  netns  = zedamigo_netns.TEST_NS_B.name
+}
+
+resource "zedamigo_tap" "TAP_B_3" {
+  name   = "tapB-3-${var.config_suffix}"
+  mtu    = "1500"
+  state  = "up"
+  group  = "kvm"
+  master = zedamigo_bridge.BRIDGE_B.name
+  netns  = zedamigo_netns.TEST_NS_B.name
+}
+
+resource "zedamigo_netns" "TEST_NS_C" {
+  name = "TEST_NS_C"
+}
+
+resource "zedamigo_bridge" "BRIDGE_C" {
+  name         = "brB-${var.config_suffix}"
+  mtu          = "1500"
+  state        = "up"
+  ipv4_address = "10.99.3.1/24"
+  netns        = zedamigo_netns.TEST_NS_C.name
+}
+
+resource "zedamigo_dhcp_server" "DHCP_C" {
+  depends_on = [zedamigo_edge_node.ENODE_001] # Because the TAPs are only moved inside the NS after the QEMU process starts.
+
+  interface  = zedamigo_bridge.BRIDGE_C.name
+  server_id  = "10.99.3.1"
+  nameserver = "9.9.9.9"
+  router     = "10.99.3.1"
+  netmask    = "255.255.255.0"
+  pool {
+    start = "10.99.3.70"
+    end   = "10.99.3.79"
+  }
+  lease_time = 86400
+  netns      = zedamigo_netns.TEST_NS_C.name
+}
+
+resource "zedamigo_tap" "TAP_C_1" {
+  name   = "tapC-1-${var.config_suffix}"
+  mtu    = "1500"
+  state  = "up"
+  group  = "kvm"
+  master = zedamigo_bridge.BRIDGE_C.name
+  netns  = zedamigo_netns.TEST_NS_C.name
+}
+
+resource "zedamigo_tap" "TAP_C_2" {
+  name   = "tapC-2-${var.config_suffix}"
+  mtu    = "1500"
+  state  = "up"
+  group  = "kvm"
+  master = zedamigo_bridge.BRIDGE_C.name
+  netns  = zedamigo_netns.TEST_NS_C.name
+}
+
+resource "zedamigo_tap" "TAP_C_3" {
+  name   = "tapC-3-${var.config_suffix}"
+  mtu    = "1500"
+  state  = "up"
+  group  = "kvm"
+  master = zedamigo_bridge.BRIDGE_C.name
+  netns  = zedamigo_netns.TEST_NS_C.name
+}
